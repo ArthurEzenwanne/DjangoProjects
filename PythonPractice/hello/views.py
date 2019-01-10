67129875f558fa1f788ffd320e6694566d040af0
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2019-01-10 15:46:20 +0100

    Add model migration

--- PythonPractice/hello/views.py
+++ PythonPractice/hello/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'hello/home.html')
 
 def about(request):
     return render(request, 'hello/about.html')
 
 def contact(request):
-    return render(request, 'hello/contact.html')    
+    return render(request, 'hello/contact.html')
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
 
 def hello_there(request, name):
     return render(

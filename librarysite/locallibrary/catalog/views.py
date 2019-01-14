d8d8ddca5565ed38fb6a3e2d48990fb2ffd964cb
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2019-01-14 13:13:49 +0100

    Create views and template files

--- librarysite/locallibrary/catalog/views.py
+++ librarysite/locallibrary/catalog/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
-
+from catalog.models import Book, Author, BookInstance, Genre, Language
+from django.views import generic
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres_fiction = Genre.objects.filter(name__icontains='fiction').count()
+    num_books_the = Book.objects.filter(title__icontains='the').count()
+    num_books_containing_genre = Book.objects.filter(genre__name__icontains='fiction').count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres_fiction': num_genres_fiction,
+        'num_books_the': num_books_the,
+        'num_books_containing_genre': num_books_containing_genre,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    """Generic class view for all books in the database."""
+    model = Book
+    paginate_by = 10        # To paginate returned records
+    # context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    """Generic class view for viewing individual books in the database."""
+    model = Book
+
+# Implement the class-based view as a function if you were not using the generic class-based detail view. 
+# def book_detail_view(request, primary_key):
+#     try:
+#         book = Book.objects.get(pk=primary_key)
+#     except Book.DoesNotExist:
+#         raise Http404('Book does not exist')
+    
+#     return render(request, 'catalog/book_detail.html', context={'book': book})    
+
+class AuthorListView(generic.ListView):
+    """Generic class view for all authors in the database."""
+    model = Author
+    paginate_by = 10        # To paginate returned records
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class view for viewing individual authors in the database."""
+    model = Author    

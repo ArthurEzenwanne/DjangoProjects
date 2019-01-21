d3636a5f2a049630cfa9bb50ce9341738f1ceff8
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2019-01-21 01:00:41 +0100

    Create tutor app models

--- schoolsapp/tutor/models.py
+++ schoolsapp/tutor/models.py
@@ -1,3 +1,57 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Tutor(models.Model):
+    """Model representing a tutor."""
+    id = models.IntegerField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=100, primary_key=True)
+    phone_num = models.IntegerField()
+    years_of_experience = models.IntegerField(default=0)
+
+    # class Meta:
+    #     ordering = ['last_name', 'first_name']
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular tutor instance."""
+        return reverse('tutor-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.first_name} {self.last_name}'
+
+class Experience(models.Model):
+    """Model representing a tutor's experience."""
+    id = models.IntegerField(primary_key=True)
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    #email_address = models.EmailField(null=False, blank=False, max_length=100, primary_key=True)
+    organization_one = models.CharField(max_length=100, null=True, blank=True)
+    job_position_one = models.CharField(max_length=100, null=True, blank=True)
+    location_one = models.CharField(max_length=100, null=True, blank=True)
+    start_date_one = models.DateField(null=True, blank=True)
+    end_date_one = models.DateField(null=True, blank=True)
+    reponsibility_one = models.CharField(max_length=1000, null=True, blank=True)
+    awards_one = models.CharField(max_length=1000, null=True, blank=True)
+    specialization_one = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.first_name} {self.last_name}'        
+
+class Education(models.Model):
+    """Model representing a tutor's education."""
+    id = models.IntegerField(primary_key=True)
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    #email_address = models.EmailField(null=False, blank=False, max_length=100, primary_key=True)
+    institution_one = models.CharField(max_length=100, null=True, blank=True)
+    course_one = models.CharField(max_length=100, null=True, blank=True)
+    qualification_one = models.CharField(max_length=50, null=True, blank=True)
+    start_date_one = models.DateField(null=True, blank=True)
+    end_date_one = models.DateField(null=True, blank=True)
+    awards_one = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.first_name} {self.last_name}'          

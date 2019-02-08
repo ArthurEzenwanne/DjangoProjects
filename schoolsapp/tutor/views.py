31af5f7e91abbe13bfa49c5c7979e0b59dc66203
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2019-02-08 20:59:49 +0100

    Perform routine house cleaning

--- schoolsapp/tutor/views.py
+++ schoolsapp/tutor/views.py
@@ -19,6 +19,20 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+def search(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    # The 'all()' is implied by default, so we can leave it out
+    num_tutors = Tutor.objects.all().count()
+        
+    context = {        
+        'num_tutors': num_tutors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
 class TutorListView(generic.ListView):
     """Class based list view for Tutor model."""
     model = Tutor

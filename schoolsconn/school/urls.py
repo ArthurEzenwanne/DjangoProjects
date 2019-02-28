a94edab17b1021e5bd9769770a1814dd884a9cbb
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2019-02-28 21:12:53 +0100

    Modify admin view template

--- schoolsconn/school/urls.py
+++ schoolsconn/school/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('schools/', views.SchoolListView.as_view(), name='schools-list'),
     path('schools/<str:slug>', views.SchoolDetailView.as_view(), name='school-detail'), # uses slug, could also use pk
     #path('accounts/profile', views.account_profile_view, name='account-profile'),   # routes to the profile view
-    path('accounts/profile', TemplateView.as_view(template_name='school/admin/profile-blue.html')),
+    path('accounts/profile', TemplateView.as_view(template_name='school/admin/user-profile.html')),
 ]

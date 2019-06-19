1f08850913d4bcef32e92d4c549db267a33b5ba4
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2019-06-19 11:48:11 +0100

    Update custom template tags definition

--- schoolsconn/school/templatetags/verbose_names.py
+++ schoolsconn/school/templatetags/verbose_names.py
@@ -5,5 +5,6 @@ register = template.Library()
 @register.simple_tag
 def get_verbose_field_name(instance, field_name):
     ''' Returns verbose name for a field. '''
-    return instance._meta.get_field(field_name).verbose_name.title()
-    #return instance._meta.get_field(field_name).verbose_name()
+    #return instance._meta.get_field(field_name).verbose_name.title()
+    # Removed the .title() so that the verbose names cames out as defined
+    return instance._meta.get_field(field_name).verbose_name
